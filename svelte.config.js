bc0dc3d291a448073dfc23160c0b99ff84df3c7c
snehith03 <snehith03@gmail.com>
2023-11-27 01:38:46 +0530

    ff

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
+export const prerender = false;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,22 +8,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: vercel({
-			// if true, will deploy the app using edge functions
-			// (https://vercel.com/docs/concepts/functions/edge-functions)
-			// rather than serverless functions
-			edge: false,
-
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		})
-	}
-};
-
+	 kit: {
+    adapter: adapter({
+      maxDuration: 60,
+    }),
+  },
+}
 export default config;
